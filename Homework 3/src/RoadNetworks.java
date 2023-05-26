70f8a5700099e77e62a112ffb1076c09d143fe57
mahithapen <116571437+mahithapen@users.noreply.github.com>
2023-05-26 10:35:05 -0400

    roadnetworks progress

--- Homework 3/src/RoadNetworks.java
+++ Homework 3/src/RoadNetworks.java
@@ -5,16 +5,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 public class RoadNetworks {
-    private int V;
+    private int V; // Number of vertices in the graph.
     private boolean[] visited;
-
+    private List<Set<Integer>> adjacencyList;
+    private Map<String, Integer> stringsToIndexes;
 
     //Let's first create a map that will represent all of our cities in one cohesive place:
 
-    private Map<String, Integer> constructMap(List<String> towns){
-
-
-    }
+    private Map<String, Integer> create(List<String> towns)
 
 
 
