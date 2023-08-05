8190a5c78048c86a1a58b42ba27ce66a411fdf77
mahithapen <116571437+mahithapen@users.noreply.github.com>
2023-08-05 09:37:18 -0400

    hw4

--- Homework4/src/CoinChange.java
+++ Homework4/src/CoinChange.java
@@ -1,3 +1,23 @@
 public class CoinChange {
+    public static void main(String[] args) {
+        int[] coins = {2,5,10};
+        int sumA = 20;
+        int sumB = 15;
+        CoinChange c = new CoinChange();
+        System.out.println(c.findCount(coins, sumA));
+        System.out.println(c.findCount(coins, sumB));
 
+    }
+    public int findCount(int[] coins, int sum) {
+        int[] dp = new int[sum + 1];
+        dp[0] = 1;
+
+        for (int coin : coins) {
+            for (int i = coin; i < sum+1; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+
+        return dp[sum];
+    }
 }
--- Homework4/src/LargestSquareOf1s.java
+++ Homework4/src/LargestSquareOf1s.java
@@ -1,3 +1,46 @@
 public class LargestSquareOf1s {
+    public static void main(String[] args) {
+        LargestSquareOf1s l = new LargestSquareOf1s();
+        int[][] a = {
+                {0, 1, 0, 1},
+                {0, 0, 1, 1},
+                {0, 1, 1, 1},
+                {0, 0, 1, 1}
+        };
+        int[][] b = {
+                {0, 1, 0, 1, 1},
+                {0, 0, 1, 1, 1},
+                {1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1},
+                {0, 1, 1, 0, 0}
+        };
+        System.out.println(l.findLargestSquare(a));
+        System.out.println(l.findLargestSquare(b));
+    }
 
+    //time complexity is O(NM)--> this is using a more brute force method
+    public int findLargestSquare(int[][] matrix) {
+        int[][] dynamicMatrix = new int[matrix.length][matrix[0].length];
+        int highestSide = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (i == 0 || j == 0) {
+                    dynamicMatrix[i][j] = 1;
+                } else {
+                    if (matrix[i][j] == 1) {
+                        int topLeft = dynamicMatrix[i-1][j-1];
+                        int top = dynamicMatrix[i-1][j];
+
+                        int left = dynamicMatrix[i][j-1];
+                        int minVal = Math.min(top, Math.min(left, topLeft));
+
+
+                        dynamicMatrix[i][j] = minVal + 1;
+                    }
+                }
+                highestSide = Math.max(highestSide, dynamicMatrix[i][j]);
+            }
+        }
+        return highestSide;
+    }
 }
