a25daf6c879a5a0b55154b7de6ad98b8bf4bc703
mahithapen <116571437+mahithapen@users.noreply.github.com>
2023-08-09 16:55:30 -0400

    homework 4 use without a comparator

--- Homework4/src/AdoptAPet.java
+++ Homework4/src/AdoptAPet.java
@@ -1,126 +1,82 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class AdoptAPet {
 
-import java.util.*;
-import java.util.Comparator;
-    abstract class Animal {
+    public static void main(String[] args) {
+        AdoptAPet solve = new AdoptAPet();
+        Pet pet = new Pet("Sadie", "dog", 4);
+        solve.addPet(pet);
+        pet = new Pet("Woof", "cat", 7);
+        solve.addPet(pet);
+        pet = new Pet("Chirpy", "dog", 2);
+        solve.addPet(pet);
+        pet = new Pet("Lola", "dog", 1);
+        solve.addPet(pet);
+        // Adoption 1
+        Person person = new Person("Bob", "dog");
+        solve.adoptAPet(person);
 
+    }
+    Queue<Pet> queueCat = new LinkedList<>();
+    Queue<Pet> queueDog = new LinkedList<>();
+
+    public void addPet(Pet p) {
+        if (p.type == "dog") {
+            queueDog.add(p);
+        } else if (p.type == "cat") {
+            queueCat.add(p);
+        }
+    }
+
+    public void adoptAPet(Person p) {
+        Pet adopted = new Pet();
+        if (p.preference == "dog") {
+            if (queueDog.isEmpty()) {
+                adopted = queueCat.poll();
+            } else {
+                adopted = queueDog.poll();
+            }
+        }
+
+        else if (p.preference == "cat") {
+            if (queueCat.isEmpty()) {
+                adopted = queueDog.poll();
+            } else {
+                adopted = queueCat.poll();
+            }
+        }
+
+        //see if the name and type print out right
+        System.out.println(adopted.name + ", " + adopted.type);
+
+    }
+
+    public static class Pet {
         String name;
-        int time;
-        public Animal(String n){
-            name = n;
-        }
-        public void setTime(int t){
+        String type;
+        int shelterTime;
 
-            time = t;
+        Pet() {
         }
-        public int getTime(){
-            return time;
+
+        Pet(String n, String t, int s) {
+            this.name = n;
+            this.type = t;
+            this.shelterTime = s;
         }
+
     }
 
-    class Dog extends Animal {
-        public Dog(String n) {super(n);}
-    }
+    public static class Person {
+        String name;
+        String preference;
 
-    class Cat extends Animal {
-        public Cat(String n) {super(n);}
-    }
-
-
-    public class AdoptAPet {
-
-        public static void main(String[] args){
-            //copied from the spec
-            Dog Sadie = new Dog("Sadie");
-            Sadie.setTime(4);
-            Cat Woof = new Cat("Woof");
-            Woof.setTime(7);
-            Dog Chirpy = new Dog("Chirpy");
-            Chirpy.setTime(2);
-            Dog Lola = new Dog("Lola");
-            Lola.setTime(1);
-
-            AdoptAPet shelter = new AdoptAPet();
-            shelter.insert(Sadie);
-            shelter.insert(Woof);
-            shelter.insert(Chirpy);
-            shelter.insert(Lola);
-
-            System.out.println(Arrays.toString(shelter.dogs.toArray()));
-            System.out.println(Arrays.toString(shelter.cats.toArray()));
-
-
-            System.out.println(shelter.adopt("dog"));
-
-
-            Cat Floofy = new Cat("Floofy");
-            shelter.insert(Floofy);
-
-            System.out.println(shelter.adopt("cat"));
-
-            System.out.println(shelter.adopt("cat"));
-
-
-            System.out.println(shelter.adopt("dog"));
-
-            System.out.println(Arrays.toString(shelter.dogs.toArray())); //[Lola]
-            System.out.println(Arrays.toString(shelter.cats.toArray())); //[]
-
+        Person(String n, String p) {
+            this.name = n;
+            this.preference = p;
         }
 
-
-        Queue<Animal> dogs = new PriorityQueue<Animal>(10, new Comparator<Dog>() {
-            public int compare(Animal d1, Animal d2) {
-                if (d1.getTime() < d2.getTime()) {
-                    return -1;
-                } else if (d1.getTime() > d2.getTime()){
-                    return 1;
-                }
-                return 0;
-            }
-        });
-        Queue<Animal> cats = new PriorityQueue<Animal>(10, new Comparator<Cat>() {
-            public int compare(Animal c1, Animal c2) {
-                if (c1.getTime() < c2.getTime()) { //older cats in front
-                    return -1;
-                } else if (c1.getTime() > c2.getTime()){
-                    return 1;
-                }
-                return 0;
-            }
-        });
-        private int time = 0;
-
-        public void insert(Animal a){
-            a.setTime(time);
-            time++;
-            if (a instanceof Dog){
-                dogs.offer(a);
-            } else if (a instanceof Cat){
-                cats.offer(a);
-            }
-        }
-
-        public String adopt(String a){
-            if (cats.isEmpty() && dogs.isEmpty()){
-                return "Sorry! No pets available right now.";
-            }
-            else if (a == "dog"){
-                if (!dogs.isEmpty()){
-                    return dogs.poll().name;
-                } else {
-                    return cats.poll().name;
-                }
-            }
-            else {
-                if (!cats.isEmpty()){
-                    return cats.poll().name;
-                } else {
-                    return dogs.poll().name;
-                }
-            }
-        }
-
-
-
     }
+
+}
